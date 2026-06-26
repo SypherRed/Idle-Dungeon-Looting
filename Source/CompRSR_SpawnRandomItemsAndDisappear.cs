77c4a9d6b58b6a5d7abab858f51fc33280b461ca
SypherRed <ext.sypher@gmail.com>
2026-06-26 23:27:23 +0200

    ref: CompRSR_SpawnRandomItemsAndDisappear to improve loot spawning logic and add validation checks

--- Source/CompRSR_SpawnRandomItemsAndDisappear.cs
+++ Source/CompRSR_SpawnRandomItemsAndDisappear.cs
@@ -1,10 +1,13 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace RimWorld;
 
 public class CompRSR_SpawnRandomItemsAndDisappear : ThingComp
 {
+    private bool spawnQueued;
+
     private CompProperties_RSR_SpawnRandomItemsAndDisappear Props =>
         (CompProperties_RSR_SpawnRandomItemsAndDisappear)props;
 
@@ -12,16 +15,61 @@ public class CompRSR_SpawnRandomItemsAndDisappear : ThingComp
     {
         base.PostSpawnSetup(respawningAfterLoad);
 
-        if (respawningAfterLoad || Props.ItemSpawnList.NullOrEmpty())
+        if (!respawningAfterLoad)
+        {
+            spawnQueued = true;
+        }
+    }
+
+    public override void CompTick()
+    {
+        base.CompTick();
+
+        if (!spawnQueued)
         {
             return;
         }
 
-        int dropCount = Rand.RangeInclusive(Props.amountOfDropsMin, Props.amountOfDropsMax);
+        spawnQueued = false;
+        SpawnLootAndDestroy();
+    }
+
+    private void SpawnLootAndDestroy()
+    {
+        if (parent.Map == null)
+        {
+            parent.Destroy();
+            return;
+        }
+
+        if (Props.ItemSpawnList.NullOrEmpty())
+        {
+            Log.Warning($"[Idle Dungeon Looting] {parent.def.defName} has no loot entries.");
+            parent.Destroy();
+            return;
+        }
+
+        List<RSR_ItemSpawnList> validEntries = Props.ItemSpawnList
+            .Where(entry => entry?.itemToSpawn != null && entry.itemSpawnWeight > 0)
+            .ToList();
+
+        if (validEntries.Count == 0)
+        {
+            Log.Warning($"[Idle Dungeon Looting] {parent.def.defName} has no valid loot entries.");
+            parent.Destroy();
+            return;
+        }
+
+        int dropCount = Rand.RangeInclusive(
+            Props.amountOfDropsMin,
+            Props.amountOfDropsMax < Props.amountOfDropsMin ? Props.amountOfDropsMin : Props.amountOfDropsMax);
+
         for (int i = 0; i < dropCount; i++)
         {
-            RSR_ItemSpawnList entry = Props.ItemSpawnList.RandomElementByWeight(x => x.itemSpawnWeight);
-            int itemCount = Rand.RangeInclusive(entry.itemAmountMin, entry.itemAmountMax);
+            RSR_ItemSpawnList entry = validEntries.RandomElementByWeight(x => x.itemSpawnWeight);
+            int itemCount = Rand.RangeInclusive(
+                entry.itemAmountMin,
+                entry.itemAmountMax < entry.itemAmountMin ? entry.itemAmountMin : entry.itemAmountMax);
 
             for (int j = 0; j < itemCount; j++)
             {
